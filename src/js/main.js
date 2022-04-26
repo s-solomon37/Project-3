60c7a817939ad693898dc7b700439dc7a19bf102
Shemariah Solomon <shemariahsolomon0@gmail.com>
2022-04-26 19:53:48 -0400

    working on next button

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
+var active = 0;
+var backButton = document.querySelector("#back");
+var nextButton = document.querySelector("#next");
+var slides = document.querySelectorAll(".slideshow > div");
 
+function previous() {
+  if (active <= 0) {
+    active = slides.length - 1;
+  } else {
+    active--;
+  }
+  console.log(active);
+}
+
+function next() {
+  active++;
+  console.log(active);
+}
+
+backButton.addEventListener("click", back);
+nextButton.addEventListener("click", next);
